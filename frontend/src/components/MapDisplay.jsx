c036cc711228978cd14a49b3e98b5b0d8fbfe9d5
Nicholas Symons <nicholas.symons@cgi.com>
2019-11-22 21:06:38 +0100

    Add basic heatmap

--- frontend/src/components/MapDisplay.jsx
+++ frontend/src/components/MapDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Source, Layer}  from 'react-map-gl';
+import {heatmapLayer} from './heatmap-style';
 
 export default class MapDisplay extends React.Component {
   constructor(props) {
@@ -12,6 +13,55 @@ export default class MapDisplay extends React.Component {
         latitude: 52.520008,
         longitude: 13.402254,
         zoom: 13
+      }, data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {
+              id: "co20",
+              index: 6,
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [13.402254, 52.520008, 0.0]
+            }
+          },
+          {
+            type: "Feature",
+            properties: {
+              id: "co20",
+              index: 6,
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [13.403254, 52.520008, 0.0]
+            }
+          },
+          {
+            type: "Feature",
+            properties: {
+              id: "co20",
+              index: 6,
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [13.403254, 52.51900, 0.0]
+            }
+          },
+          {
+            type: "Feature",
+            properties: {
+              id: "co20",
+              index: 600,
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [13.402254, 52.51900, 0.0]
+            }
+          }
+
+        ]
       },
       token: "pk.eyJ1IjoidGVhbW5pa29sYXVzIiwiYSI6ImNrM2FlYmVvNzBheDIzb21yc25xM2tqejYifQ.X50fYA7cIFaTb7Blk_IOtA"
     };
@@ -23,7 +73,13 @@ export default class MapDisplay extends React.Component {
         {...this.state.viewport}
         mapboxApiAccessToken={this.state.token}
         onViewportChange={(viewport) => this.setState({ viewport })}
-      />
+      >
+        {this.state.data && (
+          <Source type="geojson" data={this.state.data}>
+            <Layer {...heatmapLayer} />
+          </Source>
+        )}
+      </ReactMapGL>
     );
   }
 }
