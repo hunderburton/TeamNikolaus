117326996604f023413cc18f6d64635e9fdbb811
Lucas Resch <lucas.resch@cgi.com>
2019-11-23 02:30:10 +0100

    Fix CORS problem in backend

--- backend/processing/Webserver.py
+++ backend/processing/Webserver.py
@@ -1,33 +1,37 @@
-from flask import Flask,request
+from flask import Flask, request, make_response
 import ndvi
 import tileIndex
 import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def getStartCity():
-	return '{"cityName":"Berlin"}'
+    return '{"cityName":"Berlin"}'
+
 
 @app.route('/query')
 def performQuery():
-	eastFrom = (float) (request.args.get('eastFrom'))
-	eastTo = (float) (request.args.get('eastTo'))
-	northFrom = (float) (request.args.get('northFrom'))
-	northTo = (float) (request.args.get('northTo'))
-	res = (int) (request.args.get('res')) # resolution in sqm
+    eastFrom = (float)(request.args.get('eastFrom'))
+    eastTo = (float)(request.args.get('eastTo'))
+    northFrom = (float)(request.args.get('northFrom'))
+    northTo = (float)(request.args.get('northTo'))
+    res = (int)(request.args.get('res'))  # resolution in sqm
 
-	# boundingBox = [490000, 493000, 5874000, 5878000]
-	boundingBox = [eastFrom, eastTo, northFrom, northTo]
-	n1 = ndvi.NDVI(boundingBox, "2018-05-25", res)
-	tileIndexArray = n1.process()
-	#test =[item.__dict__ for item in tileIndexArray]
-	itemList = [item.__str__() for item in tileIndexArray.values()]
-	seperator = ", "
-	itemList =  seperator.join(itemList)
-	ret = '{"type":"FeatureCollection","features":['+ itemList + ']}'
-	return ret
+    # boundingBox = [490000, 493000, 5874000, 5878000]
+    boundingBox = [eastFrom, eastTo, northFrom, northTo]
+    n1 = ndvi.NDVI(boundingBox, "2018-05-25", res)
+    tileIndexArray = n1.process()
+    # test =[item.__dict__ for item in tileIndexArray]
+    itemList = [item.__str__() for item in tileIndexArray.values()]
+    seperator = ", "
+    itemList = seperator.join(itemList)
+    response = make_response('{"type":"FeatureCollection","features":[' + itemList + ']}')
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['content-type'] = 'application/json'
+    return response
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
